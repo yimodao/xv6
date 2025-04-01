14b74f4df530328eefc19ec7b7ac90727b91183e
yimodao <1343543456@qq.com>
2025-04-01 23:55:40 +0800

    remove trash files

--- user/pingpong.d
+++
@@ -1,2 +0,0 @@
-user/pingpong.o: user/pingpong.c kernel/types.h kernel/stat.h user/user.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/11/include/stddef.h
--- user/xargs.d
+++
@@ -1,1 +0,0 @@
-user/xargs.o: user/xargs.c kernel/types.h kernel/stat.h user/user.h
